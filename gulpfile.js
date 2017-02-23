6046abdb333daeafd3046bb8f1cb6c96114faa43
Tai Vu <taivu4u@gmail.com>
2017-02-23 14:55:21 -0500

    tweak for browser-sync CSS inject instead of reloading

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,18 @@ var siteUrl = __dirname.split('/pub/')[0].split('/').pop() + '.sandbox';
 
 
 // REQUIRES ________________________________________
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var compass = require('compass-importer');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
-// var imagemin = require('gulp-imagemin');
-// var pngquant = require('imagemin-pngquant');
+var gulp            = require('gulp');
+var plumber         = require('gulp-plumber');
+var sass            = require('gulp-sass');
+var compass         = require('compass-importer');
+var autoprefixer    = require('gulp-autoprefixer');
+var sourcemaps      = require('gulp-sourcemaps');
+// var imagemin     = require('gulp-imagemin');
+// var pngquant     = require('imagemin-pngquant');
 
 // pass in siteUrl to create a unique instance for multiple instances.
-var browserSync = require('browser-sync').create(siteUrl);
-    browserSync = require('browser-sync').get(siteUrl);
+var browserSync     = require('browser-sync').create(siteUrl);
+    browserSync     = require('browser-sync').get(siteUrl);
 
 
 
@@ -23,19 +24,14 @@ var browserSync = require('browser-sync').create(siteUrl);
 
 // compile sass and return stream
 gulp.task('sass', function () {
-  return gulp.src('sass/**/*.scss')
+    return gulp.src('sass/**/*.scss')
+        .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed', importer: compass}).on('error', sass.logError))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('css'));
-});
-
-// notify and reload browser after 'sass' finishes
-gulp.task('sass-watch', ['sass'], function(done) {
-  browserSync.notify('<span style="color: red;">SASS changes detected</span>, browser refreshing...');
-  browserSync.reload();
-  done();
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.stream({match: '**/*.css'}));
 });
 
 
@@ -63,12 +59,12 @@ gulp.task('watch', function(){
   });
 
   // watch for changes on these files
-  gulp.watch('sass/**/*.scss', ['sass-watch']);
+  gulp.watch('sass/**/*.scss', ['sass']);
 
   // reload on twig changes
   gulp.watch(
     [
-      '**/*.twig'
+      'templates/**/*.twig'
     ], 
     function () {
       browserSync.notify('<span style="color: red;">Twig changes detected</span>, browser refreshing...');
