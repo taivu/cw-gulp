29a29e2cd12e9728512aed3d4220b14e552d26c5
Tai Vu <taivu4u@gmail.com>
2017-02-27 14:16:44 -0500

    fix injection
    when using sourcemaps, also creates style.css.map. browser sync doesnt
    inject when it streams a file it doesnt know how to inject (non
    css/js/html).

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ gulp.task('sass', function () {
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('css'))
-        .pipe(browserSync.stream({match: '**/*.css'}));
+        .pipe(browserSync.stream({match: '**/style.css'}));
 });
 
 
