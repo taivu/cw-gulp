96b85de5e7ba2b427e19cb06c76b964c9baf8e20
Tai Vu <tai.vu@cyberwoven.com>
2017-04-13 14:59:10 -0400

    remove unused plugins (will add later when ready)

--- gulpfile.js
+++ gulpfile.js
@@ -11,12 +11,11 @@ var autoprefixer    = require('gulp-autoprefixer');
 var sourcemaps      = require('gulp-sourcemaps');
 var stylelint       = require('gulp-stylelint');
 
-// var imagemin     = require('gulp-imagemin');
-// var pngquant     = require('imagemin-pngquant');
 
 // pass in siteUrl to create a unique instance for multiple instances.
 var browsersync     = require('browser-sync').create(siteUrl);
     browsersync     = require('browser-sync').get(siteUrl);
+
 
 var config = {
   sass: { // config obj for SASS compilation ------------------------------------------------------------------
@@ -39,41 +38,14 @@ var config = {
     proxy: siteUrl,
     host: siteUrl,
     port: 4000,
-    // uncomment to customize browser-sync notifications
-    // notify: {
-    //   styles: {
-    //     top: 'auto',
-    //     bottom: '0',
-
-    //     margin: '0px',
-    //     padding: '10px',
-    //     position: 'fixed',
-    //     fontSize: '16px',
-    //     zIndex: '9999',
-    //     borderRadius: '5px 0px 0px',
-    //     color: 'white',
-    //     textAlign: 'center',
-    //     display: 'block',
-    //     width: '100%',
-    //     backgroundColor: 'rgba(60, 197, 31, 0.75)'
-
-            // defaults with tweaks
-            // display: 'block',
-            // padding: '15px',
-            // fontFamily: 'Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", Monaco, "Courier New", Courier, monospace',
-            // position: 'fixed',
-            // fontSize: '0.9em',
-            // zIndex: '9999',
-            // top: 'auto',
-            // right: '0px',
-            // bottom: '0px',
-            // borderBottomLeftRadius:' 5px',
-            // backgroundColor: 'rgb(27, 32, 50)',
-            // margin: '0px',
-            // color: 'white',
-            // textAlign: 'center',
-    //   }
-    // },
+    notify: {
+      styles: { // override some of the default styling
+        fontFamily: 'Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", Monaco, "Courier New", Courier, monospace',
+        top: 'auto',
+        bottom: '0px',
+        borderBottomLeftRadius:' 5px',
+      }
+    },
     // uncomment for static server
     // serveStatic: [{
     //     route: ['/build', '/build/css'],
@@ -93,44 +65,27 @@ var config = {
     'ios 6', 
     'android 4'
   ],
-  stylelint: {
+  stylelint: { // stylelint config ----------------------------------------------------------------------------
     "ignoreFiles": [
       "sass/modules/*.scss",
       "sass/vendors/**/*.scss"
     ],
+    "extends": "stylelint-config-standard",
     "rules": {
-      "block-no-empty": true,
-      "color-no-invalid-hex": true,
-      "declaration-colon-space-after": "always",
-      "declaration-colon-space-before": "never",
-      "function-comma-space-after": "always",
-      "function-url-quotes": "always",
-      "property-no-vendor-prefix": true,
-      "declaration-block-no-duplicate-properties": true,
-      "string-quotes": "single",
-      "value-no-vendor-prefix": true,
-      "rule-empty-line-before": [ "always-multi-line", 
-        { 
-          except: ["after-single-line-comment"],
-          ignore: ["inside-block"]
-        }
-      ],
-      // "max-empty-lines": 5,
-      // "number-leading-zero": "never",
-      //"number-no-trailing-zeros": true,
-      //"selector-list-comma-space-before": "never",
-      //"selector-list-comma-newline-after": "always",
-      // "declaration-block-single-line-max-declarations": 1,
-      // "no-descending-specificity": true,
-      // "block-closing-brace-empty-line-before": "never",
-      // "block-opening-brace-space-before": "always",
-      // "block-closing-brace-newline-after": "always",
-      // "declaration-block-trailing-semicolon": "always",
-      // "no-extra-semicolons": true,
-      // "selector-combinator-space-before": "always",
-      // "selector-combinator-space-after": "always",
-      // "selector-list-comma-newline-after": "always",
-      // "selector-descendant-combinator-no-non-space": true,
+      "number-no-trailing-zeros": null,
+      "number-leading-zero": null,
+      "at-rule-empty-line-before": [ "always", {
+        except: [
+          "after-same-name",
+          "blockless-after-same-name-blockless",
+          "first-nested",
+        ],
+        ignore: ["after-comment"],
+      } ],
+      "selector-pseudo-element-colon-notation": null,
+      "comment-whitespace-inside": null,
+      "no-extra-semicolons": true,
+      "property-no-vendor-prefix": true
     }
   }
 }
